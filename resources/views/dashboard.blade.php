3ffeddbe09b36db662cdfd60c45780f503f3f5a4
KattiaBibi <kattia1997-@hotmail.com>
2022-04-20 16:45:49 -0500

    cambios_en_el_dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -43,7 +43,7 @@
             <div class="icon">
               <i class="ion ion-stats-bars"></i>
             </div>
-            <a href="#" class="small-box-footer">Ver todos <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="{{('requerimiento')}}" class="small-box-footer">Ver todos <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
@@ -58,7 +58,7 @@
             <div class="icon">
               <i class="ion ion-person-add"></i>
             </div>
-            <a href="#" class="small-box-footer">Ver todos <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="{{('colaborador')}}" class="small-box-footer">Ver todos <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
@@ -73,7 +73,7 @@
             <div class="icon">
               <i class="ion ion-pie-graph"></i>
             </div>
-            <a href="#" class="small-box-footer">Ver todos <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="{{('servicio')}}" class="small-box-footer">Ver todos <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
