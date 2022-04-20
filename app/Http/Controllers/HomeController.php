dab098940099c7740d54422fd11810eb3924561d
KattiaBibi <kattia1997-@hotmail.com>
2022-04-20 16:43:39 -0500

    cualquier_cosa

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,40 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        $role_name = DB::table('model_has_roles')
+            ->select('roles.name AS role_name')
+            ->join('roles', 'model_has_roles.role_id', '=', 'roles.id')
+            ->where('model_id', '=', auth()->user()->id)
+            ->get()->first()->role_name;
+
+        $query = DB::table('requerimientos')->select(DB::raw('COUNT(*) AS total_requerimientos'));
+        if ($role_name !== 'Admin') {
+            $query = $query->where('usuarioencarg_id', '=', auth()->user()->id);
+        }
+        $total_requerimientos =  $query->get()->first()->total_requerimientos;
+
+        $total_citas = DB::table('citas')
+            ->select(DB::raw('COUNT(*) AS total_citas'))
+            ->where('usuario_id', '=', auth()->user()->id)
+            ->get()->first()->total_citas;
+
+        $total_colaboradores = DB::table('colaboradores')
+            ->select(DB::raw('COUNT(colaboradores.id) AS total_colaboradores'))
+            ->join('users', 'users.colaborador_id', '=', 'colaboradores.id')
+            ->join('model_has_roles', 'model_has_roles.model_id', '=', 'users.id')
+            ->join('roles', 'model_has_roles.role_id', '=', 'roles.id')
+            ->where('roles.name', '<>', 'Admin')
+            ->get()->first()->total_colaboradores;
+
+        $total_servicios = DB::table('servicios')
+            ->select(DB::raw('COUNT(*) AS total_servicios'))
+            ->get()->first()->total_servicios;
+
+        // dd($role_name, $total_requerimientos, $total_colaboradores, $total_citas, $total_servicios);
+
+        return view(
+            'dashboard',
+            compact('total_requerimientos', 'total_colaboradores', 'total_citas', 'total_servicios')
+        );
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,14 +21,14 @@
           <!-- small box -->
           <div class="small-box bg-info">
             <div class="inner">
-              <h3>150</h3>
+              <h3>{{$total_citas}}</h3>
 
               <p>Reuniones</p>
             </div>
             <div class="icon">
               <i class="ion ion-bag"></i>
             </div>
-            <a href="#" class="small-box-footer">Ver todos <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="{{('cita')}}" class="small-box-footer">Ver todos <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
@@ -36,9 +36,9 @@
           <!-- small box -->
           <div class="small-box bg-danger">
             <div class="inner">
-              <h3>53<sup style="font-size: 20px">%</sup></h3>
+              <h3>{{$total_requerimientos}}</h3>
 
-              <p>Requerimientos pendientes</p>
+              <p>Total Requerimientos</p>
             </div>
             <div class="icon">
               <i class="ion ion-stats-bars"></i>
@@ -51,7 +51,7 @@
           <!-- small box -->
           <div class="small-box bg-warning">
             <div class="inner">
-              <h3>44</h3>
+              <h3>{{$total_colaboradores}}</h3>
 
               <p>Colaboradores</p>
             </div>
@@ -66,7 +66,7 @@
           <!-- small box -->
           <div class="small-box bg-success">
             <div class="inner">
-              <h3>65</h3>
+              <h3>{{$total_servicios}}</h3>
 
               <p>Servicios</p>
             </div>
