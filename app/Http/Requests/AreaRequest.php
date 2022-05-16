6a36b3ab5cc074bed3e4fa0ad127dbf83069e37d
KattiaBibi <kattia1997-@hotmail.com>
2022-05-16 10:12:34 -0500

    Validar Areas por empresa Unique

--- app/Http/Requests/AreaRequest.php
+++ app/Http/Requests/AreaRequest.php
@@ -2,41 +2,50 @@
 
 namespace App\Http\Requests;
 
+use App\Area;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class AreaRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    return true;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            //
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
+  {
+    Validator::extend('custom_rule', function ($attribute, $value) {
+      $empresa_id = request()->input('empresa_id');
 
-            'nombre' => 'required',
+      $query = Area::join('empresa_areas', 'empresa_areas.area_id', '=', 'areas.id')
+        ->where($attribute, $value)
+        ->where('empresa_areas.empresa_id', $empresa_id);
 
-        ];
- 
-    }
+      return !$query->count();
+    });
 
-    public function messages()
-    {
-        return [
-            'nombre.required' => 'El nombre es un campo requerido',
-
+    return [
+      'nombre' => 'required|custom_rule'
     ];
-    }
+  }
+
+  public function messages()
+  {
+    return [
+      'nombre.required' => 'El nombre es un campo requerido',
+      'nombre.custom_rule' => 'El area ya existe en esta empresa'
+    ];
+  }
 }
