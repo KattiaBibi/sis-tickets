feb08d8d866668b53299fd18d2d8a34b3106aa44
KattiaBibi <kattia1997-@hotmail.com>
2022-05-22 01:06:13 -0500

    hijkhj

--- app/Http/Controllers/RequerimientoController.php
+++ app/Http/Controllers/RequerimientoController.php
@@ -294,10 +294,14 @@ class RequerimientoController extends Controller
             ->join('model_has_roles as mr', 'mr.model_id', '=', 'u.id')
             ->join('roles as r','mr.role_id','=','r.id')
 <<<<<<< HEAD
+<<<<<<< HEAD
             ->select('u.id', 'u.name', 'u.colaborador_id', 'c.nombres', 'c.apellidos')->where('mr.role_id', 1)->orWhere('mr.role_id', 2)->where('c.empresa_id', $id)->where("c.estado","=", 1)->get();
 =======
             ->select('u.id', 'u.name', 'u.colaborador_id', 'c.nombres', 'c.apellidos')->where('mr.role_id', 1)->where('c.empresa_id', $id)->where("c.estado","=", 1)->get();
 >>>>>>> parent of cb94762 (nbmhb)
+=======
+            ->select('u.id', 'u.name', 'u.colaborador_id', 'c.nombres', 'c.apellidos')->where('mr.role_id', 1)->where('mr.role_id', 2)->where('c.empresa_id', $id)->where("c.estado","=", 1)->get();
+>>>>>>> parent of 731d57b (cvbv)
 
         return $gerentes;
     }
